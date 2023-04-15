8c82b0a05c75322f31672fa7313ede9eb53ad929
Nanfahloic <yayzoy@gmail.com>
2023-04-15 18:21:34 +0100

    index update1

--- my-app/app/_layout.js
+++ my-app/app/_layout.js
@@ -1,6 +1,18 @@
 /** @format */
 
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Stack />;
 }
--- my-app/components/home/welcome/Welcome.jsx
+++ my-app/components/home/welcome/Welcome.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+/** @format */
 
-import styles from './welcome.style'
+import React from "react";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>HELLO LOICBG</Text>
+        <Text style={styles.welcomeMessage}>Find You Are Perfect Job</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChangeText={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMethod="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
